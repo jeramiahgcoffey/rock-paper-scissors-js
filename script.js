2ab08161ad06d2b27d7b1ea13c9b4e5061632899
Jeramiah Coffey <jerrygcoffey@gmail.com>
2021-10-04 23:50:13 -0500

    Added buttons for user choice

--- script.js
+++ script.js
@@ -40,15 +40,15 @@ function findWinner(playerSelection, computerSelection) {
     }
 }
 
-function playRound(computerSelection) {
-    let playerSelection;
-    while (
-        playerSelection != "rock" &&
-        playerSelection != "paper" &&
-        playerSelection != "scissors"
-    ) {
-        playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-    }
+function playRound(playerSelection, computerSelection = computerPlay()) {
+    // let playerSelection;
+    // while (
+    //     playerSelection != "rock" &&
+    //     playerSelection != "paper" &&
+    //     playerSelection != "scissors"
+    // ) {
+    //     playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+    // }
     console.log(`Computer chose: ${computerSelection}`);
     console.log(`You chose: ${playerSelection}`);
     switch (findWinner(playerSelection, computerSelection)) {
@@ -66,17 +66,37 @@ function playRound(computerSelection) {
     }
 }
 
-function game() {
-    computerScore = 0;
-    playerScore = 0;
-    while (computerScore < 3 && playerScore < 3) {
-        playRound(computerPlay());
-    }
-    if (playerScore == 3) {
-        console.log("You are the winner!");
-    } else {
-        console.log("You lose!");
-    }
+function playRock() {
+    playRound("rock");
 }
 
-game();
+function playPaper() {
+    playRound("paper");
+}
+
+function playScissors() {
+    playRound("scissors");
+}
+
+// function game() {
+//     computerScore = 0;
+//     playerScore = 0;
+//     while (computerScore < 3 && playerScore < 3) {
+//         playRound(computerPlay());
+//     }
+//     if (playerScore == 3) {
+//         console.log("You are the winner!");
+//     } else {
+//         console.log("You lose!");
+//     }
+// }
+
+// game();
+
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+rockButton.addEventListener("click", playRock);
+paperButton.addEventListener("click", playPaper);
+scissorsButton.addEventListener("click", playScissors);
